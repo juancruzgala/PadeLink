fd8fd9183e1271c99f0b11213e6e372ef864dfbf
Lucia Gauna <luciigauna@gmail.com>
2025-09-22 19:18:57 -0300

    vistas canchero, fiscal

--- PadeLink/FrmLogin.Designer.vb
+++ PadeLink/FrmLogin.Designer.vb
@@ -28,37 +28,50 @@ Partial Class Frmlogin
         Me.btnLogin = New System.Windows.Forms.Button()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label3 = New System.Windows.Forms.Label()
+        Me.lblTitulo = New System.Windows.Forms.Label()
+        Me.btnAyuda = New System.Windows.Forms.Button()
+        Me.pbLogin = New System.Windows.Forms.PictureBox()
+        CType(Me.pbLogin, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Font = New System.Drawing.Font("MS Reference Sans Serif", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(294, 42)
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Bahnschrift SemiBold", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.SteelBlue
+        Me.Label1.Location = New System.Drawing.Point(375, 103)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(184, 28)
+        Me.Label1.Size = New System.Drawing.Size(158, 29)
         Me.Label1.TabIndex = 2
         Me.Label1.Text = "Iniciar Sesion"
         '
         'txtContrasena
         '
-        Me.txtContrasena.Location = New System.Drawing.Point(299, 244)
+        Me.txtContrasena.Location = New System.Drawing.Point(456, 443)
+        Me.txtContrasena.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.txtContrasena.Name = "txtContrasena"
-        Me.txtContrasena.Size = New System.Drawing.Size(136, 22)
+        Me.txtContrasena.Size = New System.Drawing.Size(152, 26)
         Me.txtContrasena.TabIndex = 3
         '
         'txtUsuario
         '
-        Me.txtUsuario.Location = New System.Drawing.Point(299, 171)
+        Me.txtUsuario.Location = New System.Drawing.Point(456, 391)
+        Me.txtUsuario.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.txtUsuario.Name = "txtUsuario"
-        Me.txtUsuario.Size = New System.Drawing.Size(136, 22)
+        Me.txtUsuario.Size = New System.Drawing.Size(152, 26)
         Me.txtUsuario.TabIndex = 4
         '
         'btnLogin
         '
-        Me.btnLogin.Location = New System.Drawing.Point(313, 374)
+        Me.btnLogin.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnLogin.Font = New System.Drawing.Font("Bahnschrift", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnLogin.Location = New System.Drawing.Point(381, 499)
+        Me.btnLogin.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.btnLogin.Name = "btnLogin"
-        Me.btnLogin.Size = New System.Drawing.Size(75, 23)
+        Me.btnLogin.Size = New System.Drawing.Size(165, 34)
         Me.btnLogin.TabIndex = 5
         Me.btnLogin.Text = "Ingresar"
         Me.btnLogin.UseVisualStyleBackColor = True
@@ -67,35 +80,76 @@ Partial Class Frmlogin
         '
         Me.Label2.AutoSize = True
         Me.Label2.Font = New System.Drawing.Font("MS Reference Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(295, 128)
+        Me.Label2.Location = New System.Drawing.Point(317, 391)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(74, 22)
+        Me.Label2.Size = New System.Drawing.Size(102, 26)
         Me.Label2.TabIndex = 6
-        Me.Label2.Text = "Usuario"
+        Me.Label2.Text = "Usuario:"
         '
         'Label3
         '
         Me.Label3.AutoSize = True
         Me.Label3.Font = New System.Drawing.Font("MS Reference Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label3.Location = New System.Drawing.Point(295, 210)
+        Me.Label3.Location = New System.Drawing.Point(317, 441)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(107, 22)
+        Me.Label3.Size = New System.Drawing.Size(143, 26)
         Me.Label3.TabIndex = 7
-        Me.Label3.Text = "Contraseña"
+        Me.Label3.Text = "Contraseña:"
+        '
+        'lblTitulo
+        '
+        Me.lblTitulo.AutoSize = True
+        Me.lblTitulo.Font = New System.Drawing.Font("Bahnschrift", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTitulo.Location = New System.Drawing.Point(308, 37)
+        Me.lblTitulo.Name = "lblTitulo"
+        Me.lblTitulo.Size = New System.Drawing.Size(293, 43)
+        Me.lblTitulo.TabIndex = 9
+        Me.lblTitulo.Text = "🎾 PADELINK 🎾"
+        Me.lblTitulo.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'btnAyuda
+        '
+        Me.btnAyuda.Font = New System.Drawing.Font("Verdana", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnAyuda.Location = New System.Drawing.Point(848, 515)
+        Me.btnAyuda.Name = "btnAyuda"
+        Me.btnAyuda.Size = New System.Drawing.Size(36, 35)
+        Me.btnAyuda.TabIndex = 10
+        Me.btnAyuda.Text = "?"
+        Me.btnAyuda.UseVisualStyleBackColor = True
+        '
+        'pbLogin
+        '
+        Me.pbLogin.BackColor = System.Drawing.Color.Transparent
+        Me.pbLogin.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.pbLogin.ErrorImage = Global.PadeLink.My.Resources.Resources.icono_login
+        Me.pbLogin.Image = Global.PadeLink.My.Resources.Resources.icono_login
+        Me.pbLogin.InitialImage = Global.PadeLink.My.Resources.Resources.icono_login
+        Me.pbLogin.Location = New System.Drawing.Point(337, 140)
+        Me.pbLogin.Name = "pbLogin"
+        Me.pbLogin.Size = New System.Drawing.Size(231, 233)
+        Me.pbLogin.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.pbLogin.TabIndex = 8
+        Me.pbLogin.TabStop = False
         '
         'Frmlogin
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.ClientSize = New System.Drawing.Size(900, 562)
+        Me.Controls.Add(Me.btnAyuda)
+        Me.Controls.Add(Me.lblTitulo)
+        Me.Controls.Add(Me.pbLogin)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.btnLogin)
         Me.Controls.Add(Me.txtUsuario)
         Me.Controls.Add(Me.txtContrasena)
         Me.Controls.Add(Me.Label1)
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.Name = "Frmlogin"
-        Me.Text = "Ingreso"
+        Me.Text = "Inicio de Sesion"
+        CType(Me.pbLogin, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -106,4 +160,7 @@ Partial Class Frmlogin
     Friend WithEvents btnLogin As Button
     Friend WithEvents Label2 As Label
     Friend WithEvents Label3 As Label
+    Friend WithEvents pbLogin As PictureBox
+    Friend WithEvents lblTitulo As Label
+    Friend WithEvents btnAyuda As Button
 End Class
---
+++ PadeLink/RepositorioTorneos.vb
@@ -0,0 +1,14 @@
+﻿Imports System.Collections.Generic
+
+Public Class RepositorioTorneos
+    Public Shared ReadOnly Torneos As New List(Of Torneo)()
+
+    Public Shared Sub AgregarTorneo(t As Torneo)
+        If t Is Nothing Then Throw New ArgumentNullException(NameOf(t))
+        Torneos.Add(t)
+    End Sub
+
+    Public Shared Function Listar() As List(Of Torneo)
+        Return Torneos.ToList()
+    End Function
+End Class
